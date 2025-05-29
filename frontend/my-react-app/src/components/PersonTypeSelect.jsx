54e9570efea95018327810fedf2d27b77eac4cee
utkuagus <utkuagus9@gmail.com>
2025-05-29 16:16:09 +0300

    style changes

--- frontend/my-react-app/src/components/PersonTypeSelect.jsx
+++ frontend/my-react-app/src/components/PersonTypeSelect.jsx
@@ -1,22 +1,24 @@
-import { useEffect } from 'react'
-import '../css/Login.css'
+import { useEffect } from "react";
+import "../css/Login.css";
 
 export default function PersonTypeSelect(props) {
-
-    function setPersonType(type) {
-        if(type == "student") {
-            props.setIsStudent(true)
-            props.setIsPersonTypeSelected(true)
-        } else if(type == "instructor") {
-            props.setIsStudent(false)
-            props.setIsPersonTypeSelected(true)
-        }
+  function setPersonType(type) {
+    if (type == "student") {
+      props.setIsStudent(true);
+      props.setIsPersonTypeSelected(true);
+    } else if (type == "instructor") {
+      props.setIsStudent(false);
+      props.setIsPersonTypeSelected(true);
     }
+  }
 
-    return (
-        <div className='flex center'>
-            <button onClick={() => setPersonType("student")}>student</button>
-            <button onClick={() => setPersonType("instructor")}>instructor</button>
-        </div>
-    )
+  return (
+    <div className="flex center column gap-large">
+      <div style={{ fontSize: 30 }}>Who are you?</div>
+      <div className="flex gap-large">
+        <button onClick={() => setPersonType("student")}>student</button>
+        <button onClick={() => setPersonType("instructor")}>instructor</button>
+      </div>
+    </div>
+  );
 }
